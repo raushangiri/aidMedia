07d4fe62dc1502a16660acaa845b31d28de03aa6
raushangiri <raushangiri.raj@gmail.com>
2023-05-01 15:27:29 +0530

    commited

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 // router page required here
 app.use(require("./router/auth"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "./FrontEnd/build")));
 app.get("*", function (req, res) {
